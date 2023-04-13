993bf652442c351bb373179f745c0bc8271ee128
Julius Ojala Ojakaa <ojayallday@gmail.com>
2023-04-13 23:59:22 +0300

    Full code and structure

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React , {useState} from "react";
+
+function App() {
+  const [name , setName] = useState("")
+  const [headingText , setHeadingText] = useState("")
+  function handleChange(event){
+    console.log(event.target.value)
+    setName(event.target.value)
+  }
+  function handleClick(event){
+    setHeadingText(name)
+  }
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+      <input 
+       onChange={handleChange} 
+       type="text" 
+       placeholder="What's your name?" 
+       value={name}
+       />
+      <button  >Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
